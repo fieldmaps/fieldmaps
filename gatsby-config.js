f87793aff1cb321ec6df5b5738f7c1da2fbc0a96
Max Malynowsky <maxmalynowsky@gmail.com>
2019-11-27 23:13:13 -0700

    modify site manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Fieldmaps',
+    title: 'Fieldmaps.io',
     description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+      'Mobile, offline, interactive reference maps for humanitarian use',
     author: '@gatsbyjs',
   },
   plugins: [
@@ -22,11 +22,11 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Fieldmaps.io',
+        short_name: 'Fieldmaps.io',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#78B5D6',
+        theme_color: '#78B5D6',
         display: 'minimal-ui',
         icon: 'src/images/icon.svg',
       },
