4512ee24c51e3f2b080e19c0b5269bc1f0d0c401
Max Malynowsky <github@maxmalynowsky.com>
2021-01-08 18:34:47 +0100

    use correct react-helmet syntax

--- src/components/common/metadata.tsx
+++ src/components/common/metadata.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 
 const url = 'https://fieldmaps.io/';
 const img = 'https://fieldmaps.io/img/atlas-ssd-preview.png';
