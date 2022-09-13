138bb92bf1e12149a77c446ca49401af82820da0
Max Malynowsky <github@maxmalynowsky.com>
2022-09-12 20:56:34 -0400

    update ADM0 page

--- src/components/content/data-adm0.tsx
+++ src/components/content/data-adm0.tsx
@@ -21,17 +21,12 @@ export default () => {
       <p>
         Updated: {new Date(data[0]?.date).toLocaleDateString('en-GB', options)}
       </p>
-      <h2>Detailed</h2>
       <p>
-        Very high resolution boundaries using{' '}
-        <a href="https://osmdata.openstreetmap.de/data/land-polygons.html">
-          OpenStreetMap
-        </a>{' '}
-        for coastline data,{' '}
+        Very high resolution boundaries combining coastline data with{' '}
         <a href="https://hiu.state.gov/data/">
           U.S. Department of State Large Scale International Boundaries (LSIB)
         </a>{' '}
-        for international boundaries, and{' '}
+        for international boundaries and{' '}
         <a href="https://unstats.un.org/unsd/methodology/m49/overview/">
           United Nations Statistics Division M49
         </a>{' '}
@@ -40,10 +35,12 @@ export default () => {
         technical information on how this layer is made.
       </p>
       <p>
-        This ADM0 layer comes in several versions to represent different world
-        views of disputed areas. Starting with the "All" version, areas are
-        dissolved together based on varying international recognition. The
-        following preset world views are available for download:
+        International boundaries are built using either OpenStreetMap or U.S.
+        Geological Survey coastlines. Within each set, ADM0 layers comes in
+        several versions to represent different world views of disputed areas.
+        Starting with the "All" version, areas are dissolved together based on
+        varying international recognition. The following preset world views are
+        available for download:
       </p>
       <ul>
         <li>
@@ -60,7 +57,7 @@ export default () => {
           .
         </li>
         <li>
-          <b>All</b>: A conservative world view that dissagregates disputed
+          <b>All</b>: A conservative world view that dissagregates all disputed
           areas. Useful if applying individual customization outside the presets
           generated here.
         </li>
@@ -73,13 +70,61 @@ export default () => {
           line layers above for cartographic applications.
         </li>
       </ul>
-      <TableDest adm0 data={data.filter(x => x.grp === 'detailed')} />
-      <h2>Simplified</h2>
+      <h2>OpenStreetMap (ODbL)</h2>
       <p>
-        The following uses OpenStreetMap simplified coastlines (~30MB) instead
-        of originals (&gt;1GB), LSIB layer is unchanged.
+        Default version, preferred for making web maps as it aligns with other
+        OpenStreetMap derived basemaps and data.
       </p>
-      <TableDest adm0 data={data.filter(x => x.grp === 'simplified')} />
+      <p>
+        <b>Coastline data</b>:{' '}
+        <a href="https://osmdata.openstreetmap.de/data/land-polygons.html">
+          Land polygons
+        </a>{' '}
+        (
+        <a href="https://osmdata.openstreetmap.de/download/land-polygons-complete-4326.zip">
+          download
+        </a>
+        )
+      </p>
+      <p>
+        <b>Attribution</b>: FieldMaps, U.S. Department of State, OpenStreetMap
+      </p>
+      <p>
+        <b>License</b>:{' '}
+        <a href="https://opendatacommons.org/licenses/odbl/">
+          Open Data Commons Open Database License
+        </a>{' '}
+        (ODbL)
+      </p>
+      <p>
+        <b>Conditions</b>: Derived work must include attributions, be offered
+        under the same license, and keep open access to the data
+      </p>
+      <TableDest adm0 data={data.filter(x => x.grp === 'osm')} />
+      <h2>U.S. Geological Survey (Public Domain)</h2>
+      <p>
+        Specialty version, uses sources where intellectual property and related
+        rights in the data are absent.
+      </p>
+      <p>
+        <b>Coastline data</b>:{' '}
+        <a href="https://rmgsc.cr.usgs.gov/gie/">Global Islands Database</a> (
+        <a href="https://rmgsc.cr.usgs.gov/outgoing/ecosystems/Global/USGSEsriWCMC_GlobalIslands_v3.mpk">
+          download
+        </a>
+        )
+      </p>
+      <p>
+        <b>Attribution</b>: FieldMaps, U.S. Department of State, U.S. Geological
+        Survey, Natural Earth
+      </p>
+      <p>
+        <b>License</b>: Public Domain
+      </p>
+      <p>
+        <b>Conditions</b>: None
+      </p>
+      <TableDest adm0 data={data.filter(x => x.grp === 'usgs')} />
       <p>
         Download metadata tables as: <a href={dataUrl + '.json'}>json</a> |{' '}
         <a href={dataUrl + '.csv'}>csv</a> |{' '}
