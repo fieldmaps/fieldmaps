a8acb77001f34f88ab34c0e53cc645b014bc242e
Max Malynowsky <github@maxmalynowsky.com>
2025-07-26 16:34:24 +0200

    add parquet format support

--- src/lib/components/TableDest.svelte
+++ src/lib/components/TableDest.svelte
@@ -18,6 +18,7 @@
     <thead>
       <tr>
         <th>{adm0 ? 'World View' : 'Level'}</th>
+        <th>GeoParquet (cloud-native)</th>
         <th>Geopackage (QGIS)</th>
         <th>Geodatabase (ArcGIS)</th>
       </tr>
@@ -27,6 +28,7 @@
         {#if x.wld !== 'land'}
           <tr>
             <td>{x.adm === 0 ? wlds[x.wld] : 'ADM 0-' + x.adm}</td>
+            <td><a href={x.a_parquet}>Parquet</a></td>
             <td><a href={x.a_gpkg}>GPKG</a></td>
             <td><a href={x.a_gdb}>GDB</a></td>
           </tr>
